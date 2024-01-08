ba110490bafb021db7317fa4781d0098301bc9d2
kaustubh-555 <kaustubh517suroshi@gmail.com>
2024-01-08 22:21:41 +0530

    login

--- public/scripts/loginscript.js
+++ public/scripts/loginscript.js
@@ -3,7 +3,7 @@ loginBtn.addEventListener('click',async (e)=>{
     e.preventDefault();
     let usernamefeild=document.getElementById("username").value;
     let passwordfeild=document.getElementById("password").value;
-    await fetch('http://localhost:3500/login',{
+    await fetch('/login',{
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body : JSON.stringify({
